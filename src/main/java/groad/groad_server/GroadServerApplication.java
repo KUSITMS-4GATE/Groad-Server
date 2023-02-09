35987933a09298c2172f83dda28943ed3964ed4f
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-02-10 02:34:46 +0900

    [CHORE] 개발 환경 설정 #2

---
+++ src/main/java/groad/groad_server/GroadServerApplication.java
@@ -0,0 +1,13 @@
+package groad.groad_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroadServerApplication.class, args);
+	}
+
+}
