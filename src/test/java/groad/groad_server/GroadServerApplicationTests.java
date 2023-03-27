e13f7b9831bc8d2bdc42ec9852c05426ec2d5070
knmy0925@gmail.com <85796588+goseungwon@users.noreply.github.com>
2023-03-28 02:10:32 +0900

    Chore: 프로젝트 생성 (#3)
    * [CHORE] 개발 환경 설정 #2
    
    * [CHORE] CI 스크립트 생성
    
    * [CHORE] Secret yml 연결 추가 #2
    
    * Update CI.yml
    
    * Update CI.yml
    
    * [CHORE] jasypt 암호화 설정 #2
    
    * [CHORE] jasypt 암호화 오류 수정, gitignore 수정 #2
    
    * [CHORE] gitignore 수정 #2
    
    * [CHORE] gitignore 수정 #2
    
    * Update CI.yml
    
    * [CHORE] jasypt 암호화 오류 해결 #2
    
    * Chore: ci 스크립트 수정
    
    vm 전역변수를 넣어주는 방식 변경으로 인한 커맨드라인 수정
    
    * [CHORE] CI scripts 수정 #2
    
    * [CHORE] CI scripts 수정 #2
    
    * [CHORE] ubuntu 버전 수정 #2
    
    * [CHORE] CD 스크립트 작성 #2
    
    * [Chore] CD 스크립트 수정 #2
    
    * [CHORE] Dockerfile 작성 #2
    
    * [CHORE] JAR_FILE 경로 변경 #2
    
    * [CHORE] update docker image path  #2
    
    * [CHORE] docker-compose 제외 #2
    
    * [CHORE] CD스크립트 작업 완료 후 디버그 코드 주석처리 #2
    
    ---------
    
    Co-authored-by: dl-00-e8 <sso07012@mail.hongik.ac.kr>
    Co-authored-by: Jeongjin Lee <sso07012@gmail.com>

---
+++ src/test/java/groad/groad_server/GroadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package groad.groad_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
