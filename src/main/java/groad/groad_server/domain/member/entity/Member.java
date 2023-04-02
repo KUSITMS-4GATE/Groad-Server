46ed3c519259f7086dd676fe6e55a28750a4cd2d
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-04-02 22:27:24 +0900

    [CHORE] Validation 관련 dependency 추가 & Entity Null 여부 검증 어노테이션 추가 #8

--- src/main/java/groad/groad_server/domain/member/entity/Member.java
+++ src/main/java/groad/groad_server/domain/member/entity/Member.java
@@ -4,6 +4,7 @@ package groad.groad_server.domain.member.entity;
 import groad.groad_server.domain.common.BaseTimeEntity;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "member")
@@ -14,12 +15,15 @@ public class Member extends BaseTimeEntity {
     @Column(name = "id")
     private Long id;
 
+    @NotNull
     @Column(name = "name")
     private String name;
 
+    @NotNull
     @Column(name = "email", unique = true)
     private String email;
 
+    @NotNull
     @Column(name = "authority")
     private String authority;
 
--- src/main/java/groad/groad_server/domain/review/entity/Review.java
+++ src/main/java/groad/groad_server/domain/review/entity/Review.java
@@ -3,6 +3,7 @@ package groad.groad_server.domain.review.entity;
 import groad.groad_server.domain.common.BaseTimeEntity;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "review")
@@ -13,12 +14,15 @@ public class Review extends BaseTimeEntity {
     @Column(name = "id")
     private Long id;
 
+    @NotNull
     @Column(name = "member_id")
     private Long member_id;
 
+    @NotNull
     @Column(name = "title")
     private String title;
 
+    @NotNull
     @Column(name = "contents")
     private String contents;
 }
