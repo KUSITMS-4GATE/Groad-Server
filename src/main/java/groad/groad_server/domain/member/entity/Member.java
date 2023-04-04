e596c984ffe5a304ec000e0ab8dfc2d8beadee22
dl-00-e8 <sso07012@mail.hongik.ac.kr>
2023-04-04 22:02:54 +0900

    [CHORE] Member & Review Entity에 Softdelete 및 Builder 추가 #8

--- src/main/java/groad/groad_server/domain/member/entity/Member.java
+++ src/main/java/groad/groad_server/domain/member/entity/Member.java
@@ -2,12 +2,20 @@ package groad.groad_server.domain.member.entity;
 
 
 import groad.groad_server.domain.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "member")
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE member SET deleted_at = NOW() where id = ?")
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
 public class Member extends BaseTimeEntity {
 
     @Id
@@ -29,4 +37,13 @@ public class Member extends BaseTimeEntity {
 
     @Column(name = "university")
     private String university;
+
+    @Builder
+    public Member(Long id, String name, String email, String authority, String university) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.authority = authority;
+        this.university = university;
+    }
 }
--- src/main/java/groad/groad_server/domain/review/entity/Review.java
+++ src/main/java/groad/groad_server/domain/review/entity/Review.java
@@ -1,12 +1,20 @@
 package groad.groad_server.domain.review.entity;
 
 import groad.groad_server.domain.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "review")
+@Where(clause = "deleted_at IS NULL")
+@SQLDelete(sql = "UPDATE review SET deleted_at = NOW() where id = ?")
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
 public class Review extends BaseTimeEntity {
 
     @Id
@@ -25,4 +33,11 @@ public class Review extends BaseTimeEntity {
     @NotNull
     @Column(name = "contents")
     private String contents;
+
+    @Builder
+    public Review(Long id, Long member_id, String contents) {
+        this.id = id;
+        this.member_id = member_id;
+        this.contents = contents;
+    }
 }
